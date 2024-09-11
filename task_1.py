937afb54d5b6c9dde974d29a91861143b2ced5d3
Botora77# <sakhvadzeirakli@gmail.com>
2024-09-11 20:16:57 +0400

    done

---
+++ task_1.py
@@ -0,0 +1,11 @@
+# begin
+
+name = input("enter your name:")
+surname = input("enter your surname:")
+age = input("enter your age:")
+
+print(f"გამარჯობა,{name} {surname}, თქვენ ხართ {age} წლის")
+
+
+
+
